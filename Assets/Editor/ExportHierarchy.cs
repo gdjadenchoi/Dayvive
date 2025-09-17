ab3ae18ffb7f5c4d7900726fc9fc7c970065ee20
Jaden Choi <kei0604@naver.com>
2025-09-18 04:25:23 +0900

    projectile 발사/사거리 동기화 완료

---
+++ Assets/Editor/ExportHierarchy.cs
@@ -0,0 +1,31 @@
+﻿// Assets/Editor/ExportHierarchy.cs
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class ExportHierarchy
+{
+    [MenuItem("Tools/Dayvive/Export Hierarchy (active scene)")]
+    public static void Export()
+    {
+        var scene = SceneManager.GetActiveScene();
+        var roots = scene.GetRootGameObjects();
+        var sb = new StringBuilder();
+        sb.AppendLine($"# Hierarchy – {scene.name}");
+        foreach (var go in roots)
+            Dump(go.transform, 0, sb);
+        var path = "Assets/Hierarchy_export.txt";
+        File.WriteAllText(path, sb.ToString(), Encoding.UTF8);
+        AssetDatabase.Refresh();
+        Debug.Log($"Exported hierarchy to: {path}");
+    }
+
+    static void Dump(Transform t, int depth, StringBuilder sb)
+    {
+        sb.Append(' ', depth * 2).Append("└─ ").AppendLine(t.name);
+        for (int i = 0; i < t.childCount; i++)
+            Dump(t.GetChild(i), depth + 1, sb);
+    }
+}
